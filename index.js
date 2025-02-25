ea2ff7c836a43acd04b28e18b6e58406b7eebf34
knyazkin maxim <knyazkin.maxim.626@gmail.com>
2025-02-25 21:36:45 +0300

    Mobile 1.4

---
+++ index.js
@@ -0,0 +1,34 @@
+/////////Кнопки на слайдере/////////////
+const slides = document.querySelectorAll('.buy');
+const circles = document.querySelectorAll('.circle');
+
+function resetCircles() {
+    circles.forEach(circle => {
+      circle.classList.remove('circle-active');
+    });
+  }
+  
+  const observerCallback = (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const index = Array.from(slides).indexOf(entry.target);
+        resetCircles(); 
+        if (circles[index]) {
+          circles[index].classList.add('circle-active'); 
+        }
+      }
+    });
+  };
+  
+  const observer = new IntersectionObserver(observerCallback, {
+    root: null, 
+    threshold: 0.5
+  });
+  
+  slides.forEach(slide => {
+    observer.observe(slide);
+  });
+
+
+
+
