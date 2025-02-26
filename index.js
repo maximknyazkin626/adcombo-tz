6c2fba4ee794d7e8e10a9f8d03adf39d35e7b22c
knyazkin maxim <knyazkinmaxim626@gmail.com>
2025-02-26 16:46:20 +0300

    Mobile 1.5

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
 /////////Кнопки на слайдере/////////////
-const slides = document.querySelectorAll('.buy');
-const circles = document.querySelectorAll('.circle');
+const slides = document.querySelectorAll(".buy");
+const circles = document.querySelectorAll(".circle");
 
 function resetCircles() {
-    circles.forEach(circle => {
-      circle.classList.remove('circle-active');
-    });
-  }
-  
-  const observerCallback = (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const index = Array.from(slides).indexOf(entry.target);
-        resetCircles(); 
-        if (circles[index]) {
-          circles[index].classList.add('circle-active'); 
-        }
+  circles.forEach((circle) => {
+    circle.classList.remove("circle-active");
+  });
+}
+
+const observerCallback = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      const index = Array.from(slides).indexOf(entry.target);
+      resetCircles();
+      if (circles[index]) {
+        circles[index].classList.add("circle-active");
       }
-    });
-  };
-  
-  const observer = new IntersectionObserver(observerCallback, {
-    root: null, 
-    threshold: 0.5
+    }
   });
-  
-  slides.forEach(slide => {
-    observer.observe(slide);
-  });
+};
 
+const observer = new IntersectionObserver(observerCallback, {
+  root: null,
+  threshold: 0.5,
+});
 
-
-
+slides.forEach((slide) => {
+  observer.observe(slide);
+});
