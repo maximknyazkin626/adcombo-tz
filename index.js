47cba5518c94bebc2ce5f88be7a7d7902f2d9cb1
knyazkin maxim <knyazkin.maxim.626@gmail.com>
2025-02-26 21:18:46 +0300

    Desktop 1.5

--- index.js
+++ index.js
@@ -28,3 +28,38 @@ const observer = new IntersectionObserver(observerCallback, {
 slides.forEach((slide) => {
   observer.observe(slide);
 });
+
+
+///////////////Таймер////////////////////
+let totalTime = 10 * 60; // 10 минут в секундах
+
+        function startTimer() {
+            let timeLeft = totalTime;
+
+            const interval = setInterval(() => {
+                // Вычисляем минуты и секунды
+                const hours = Math.floor(timeLeft / 3600);
+                const minutes = Math.floor(timeLeft / 60);
+                const seconds = timeLeft % 60;
+
+                // Форматируем минуты и секунды, добавляя ноль перед единичными значениями
+                const formattedHours = String(hours).padStart(2, '0');
+                const formattedMinutes = String(minutes).padStart(2, '0');
+                const formattedSeconds = String(seconds).padStart(2, '0');
+
+                // Обновляем текст таймера
+                document.getElementById('sale-timer').textContent = `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+
+                // Уменьшаем оставшееся время
+                timeLeft--;
+
+                // Проверяем, истекло ли время
+                if (timeLeft < 0) {
+                    clearInterval(interval); // Очищаем интервал
+                    startTimer(); // Перезапускаем таймер
+                }
+            }, 1000); // Обновляем каждую секунду
+        }
+
+        // Запускаем таймер при загрузке страницы
+        window.onload = startTimer;
